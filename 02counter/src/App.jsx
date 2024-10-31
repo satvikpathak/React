31a9f0213fa7cd30098eb1d77289597e34510d34
satvikpathak007 <satvikpathak007@gmail.com>
2024-11-01 02:54:47 +0530

    r02

---
+++ 02counter/src/App.jsx
@@ -0,0 +1,29 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  const [counter, setCounter] = useState(15);
+
+  const addValue = () => {
+      setCounter(counter + 1);
+      console.log(counter);
+  }
+
+  const removeValue = () => {
+      setCounter(counter - 1);
+      console.log(counter);
+  }
+
+    return (
+    <>
+      <h1>Course with hitesh sir {counter}</h1>
+      <h2>counter value: {counter}</h2>
+      <button onClick={addValue}>Add Value</button> {" "}
+      <button onClick={removeValue}>Remove Value</button>
+      <p>footer: {counter}</p>
+    </>
+  )
+}
+
+export default App
