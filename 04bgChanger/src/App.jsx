255279b34826cff264a39a00d826a5fdc7e84c1d
satvikpathak007 <satvikpathak007@gmail.com>
2024-11-02 01:13:44 +0530

    r

---
+++ 04bgChanger/src/App.jsx
@@ -0,0 +1,28 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [color, setColor] = useState('#808000')
+
+  return (
+    <div className='w-screen h-screen' style={{backgroundColor: color}}>
+      <div className='fixed flex flex-wrap justify-center bottom-12 inset-x-0 px-2'>
+        <div className='flex flex-wrap justify-center gap-3 shadow-lg bg-white px-3 py-2 rounded-3xl'>
+          <button onClick={() => setColor('red')} className='outline-none px-4 py-1 rounded-full text-black shadow-lg bg-red-500'>
+            Red
+          </button>
+          <button onClick={() => setColor('blue')} className='outline-none px-4 py-1 rounded-full text-black shadow-lg bg-blue-500'>
+            Blue
+          </button>
+          <button onClick={() => setColor('green')} className='outline-none px-4 py-1 rounded-full text-black shadow-lg bg-green-500'>
+            Green
+          </button>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default App
